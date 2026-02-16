4f548f330bf49f7e04dcec1406e134bc3607da5b
Legend21-create <troyreid457@gmail.com>
2026-02-15 20:09:02 -0500

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,12 +1,27 @@
 // CREME THEORY - CENTRAL INVENTORY
-// true = Available | false = Sold Out
+// Use true for Available | false for Sold Out
 const InventoryData = {
-    "Signature 6\" Cake": { inStock: true, price: 6500 },
-    "Velvet Theory": { inStock: false, price: 7200 },
-    "Heritage Croissant": { inStock: true, price: 850 },
-    "Classic Croissant": { inStock: true, price: 850 },
-    "Chocolate Ganache": { inStock: true, price: 1200 }
+    "Signature 6\" Cake": { 
+        "inStock": true, 
+        "price": 6500 
+    },
+    "Velvet Theory": { 
+        "inStock": false, 
+        "price": 7200 
+    },
+    "Heritage Croissant": { 
+        "inStock": true, 
+        "price": 850 
+    },
+    "Classic Croissant": { 
+        "inStock": true, 
+        "price": 850 
+    },
+    "Chocolate Ganache": { 
+        "inStock": true, 
+        "price": 1200 
+    }
 };
 
-// This function makes the data available to your other scripts
+// This makes the data accessible across your Menu, Order Form, and Admin pages
 window.CremeInventory = InventoryData;
